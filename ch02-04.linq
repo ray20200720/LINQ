75e9e4cee84c4e48a726bdf005acdb9cb7b2420b
ray20200720 <70304042+ray20200720@users.noreply.github.com>
2021-03-24 00:25:13 +0800

    Update

---
+++ ch02-04.linq
@@ -0,0 +1,52 @@
+<Query Kind="Program" />
+
+/* 	
+	Title: Implicitly Typed Local Variables
+	Date: 2021/3/23
+*/
+
+void Main()
+{
+	// implicitly typed local variable declarations 
+	// 隱含類別區域變數宣告
+	var anInt = 1;
+	var aString = "Testing";
+	var listContact = new List<Contact>();
+	var intArray = new int[] { 0, 1, 2, 3, 4 };
+
+	Console.WriteLine("{0} {1} {2} {3}", anInt, aString, listContact, intArray);
+
+	// the above are identical to the following declarations
+	// 下列為上述已辨識的型別宣告
+	int anIntOld = 1;
+	string aStringOld = "Testing";
+	List<Contact> listContactOld = new List<Contact>(); 
+	int[] intArrayOld = new int[] { 0, 1, 2, 3, 4 };
+
+	Console.WriteLine("{0} {1} {2} {3}", anIntOld, aStringOld, listContactOld, intArrayOld);
+
+	var list = new List<Contact> 
+	{
+		new Contact 
+		{
+			LastName = "Magennis",
+			DateOfBirth = new DateTime(1973, 12, 09)
+		},
+		new Contact 
+		{
+			LastName = "Doherty",
+			DateOfBirth = new DateTime(1978, 1, 05)
+		}
+	};
+
+	foreach (var item in list)
+	{
+		Console.WriteLine("{0} {1}", item.LastName, item.DateOfBirth);
+	}
+}
+
+class Contact
+{
+	public string LastName { get; set; }
+	public DateTime DateOfBirth { get; set; }
+}
--- ex02-03.cs
+++ ex02-03.cs
@@ -1,4 +1,4 @@
-/* 	
+/*
 	Title: 集合初始化設定
 	Date: 2021/3/23
 */
@@ -6,18 +6,47 @@
 void Main()
 {
 	// old initialization syntax => multiple statements
-	Contact contactOld = new Contact();
-	contactOld.LastName = "Magennis";
-	contactOld.DateOfBirth = new DateTime(1973, 12, 09);
-	Console.WriteLine("{0}  ({1})", contactOld.LastName, contactOld.DateOfBirth);
+	List<string> stringsOld = new List<string>();
+	stringsOld.Add("string 1");
+	stringsOld.Add("string 2");
+
+	foreach (var item in stringsOld)
+	{
+		Console.WriteLine("{0}", item);
+	}
 	
 	// new initialization syntax => single statement
-	Contact contactNew = new Contact 
+	List<string> stringsNew = new List<string> 
 	{
-		LastName = "Magennis",
-		DateOfBirth = new DateTime(1973, 12, 09)
+		"new string 1",
+		"new string 2"
 	};
-	Console.WriteLine("{0}  ({1})", contactNew.LastName, contactNew.DateOfBirth);
+
+	foreach (var item in stringsNew)
+	{
+		Console.WriteLine("{0}", item);
+	}
+
+	// combining object and collection initialization
+	// create a list of contacts and add two records. 
+	List<Contact> list = new List<Contact>
+	{
+		new Contact 
+		{
+			LastName = "Magennis",
+			DateOfBirth = new DateTime(1973, 12, 09)
+		},
+		new Contact 
+		{
+			LastName = "Doherty",
+			DateOfBirth = new DateTime(1978, 1, 05)
+		}
+	};
+
+	foreach (var item in list)
+	{
+		Console.WriteLine("{0} ({1})", item.LastName, item.DateOfBirth);
+	}
 }
 
 class Contact
